9b0bd979bfd27fc058ae8631bd6681faddcd2358
nurulakbaral <nurulakbaral@gmail.com>
2022-08-04 07:55:41 +0700

    refactor: e2e testing

--- docs/docs-react-e2e/src/integration/app.spec.ts
+++ docs/docs-react-e2e/src/integration/app.spec.ts
@@ -1,39 +1,19 @@
+import { TBreakpointsTract } from './types'
 describe('docs-react', () => {
   const HEIGHT_VIEWPORT = 1000
   /**
    * Default Breakpoints (Media: MIN)
    */
-  const breakpointsDefaultMinFirst = [
-    {
-      width: 280,
-      value: 'val: base',
-    },
-    {
-      width: 350,
-      value: 'val: xs',
-    },
-    {
-      width: 576,
-      value: 'val: sm',
-    },
-    {
-      width: 768,
-      value: 'val: md',
-    },
-    {
-      width: 1080,
-      value: 'val: lg',
-    },
-    {
-      width: 1280,
-      value: 'val: xl',
-    },
-  ]
+
   beforeEach(() => cy.visit('/__test__'))
   it('should display the appropriate value for Default Breakpoints (Media: MIN)', () => {
-    breakpointsDefaultMinFirst.forEach(({ width, value }) => {
-      cy.viewport(width, HEIGHT_VIEWPORT)
-      cy.get('[data-testid=db-min-first').contains(value)
-    })
+    cy.fixture('bd-min-first.json')
+      .its('data')
+      .then((list: TBreakpointsTract) => {
+        list.forEach(({ width, value }) => {
+          cy.viewport(width, HEIGHT_VIEWPORT)
+          cy.get('[data-testid=db-min-first').contains(value)
+        })
+      })
   })
 })
