8b6cf9eaf2460364316ce959bac16fdeea30f0c9
nurulakbaral <nurulakbaral@gmail.com>
2022-08-08 11:23:24 +0700

    test(utils): add complete test for utils

---
+++ packages/react-resval/__test__/utils.spec.ts
@@ -0,0 +1,32 @@
+import { isEmptyObject, isArrayOfNumber, isArrayOfCSSUnits } from '../src/utils'
+import { CSSUnits } from '../src/constants'
+
+describe('Check the `isEmptyObject` utility so that it gives the expected output.', () => {
+  test('should return true, because empty object', () => {
+    expect(isEmptyObject({})).toBe(true)
+  })
+  test('should return false, because not empty object', () => {
+    expect(isEmptyObject({ name: 'Reval' })).toBe(false)
+    expect(isEmptyObject({ name: 'Reval', age: 12 })).toBe(false)
+  })
+})
+describe('Check the `isArrayOfNumber` utility so that it gives the expected output.', () => {
+  test('should return true, because array of number  and not NaN', () => {
+    expect(isArrayOfNumber([1, 2, 3, 4])).toBe(true)
+    expect(isArrayOfNumber([1.1, 2.2, 3.91, 3.1787])).toBe(true)
+    expect(isArrayOfNumber([1, 2, 3, NaN])).not.toBe(true)
+  })
+})
+
+describe('Check the `isArrayOfCSSUnits` utility so that it gives the expected output.', () => {
+  test('should return true, because array of CSSUnits', () => {
+    expect(isArrayOfCSSUnits(['100px', '200px', '300px', '400px'])).toBe(true)
+    expect(isArrayOfCSSUnits(CSSUnits.map((unit) => `100${unit}`))).toBe(true)
+  })
+  test('should return false, because not array of CSSUnits', () => {
+    expect(isArrayOfCSSUnits(['100pxs', '200rempx', '300emem'])).toBe(false)
+    expect(isArrayOfCSSUnits(['pxs', 'rempx', 'emem'])).toBe(false)
+    expect(isArrayOfCSSUnits(['px', 'rem', 'em'])).toBe(false)
+    expect(isArrayOfCSSUnits([...CSSUnits])).toBe(false)
+  })
+})
--- packages/react-resval/src/utils.ts
+++ packages/react-resval/src/utils.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { TBaseObject } from './types'
+import type { TBaseObject, TCSSAbsoluteUnits, TCSSRelativeUnits } from './types'
 
 export function isEmptyObject<TTypeObject extends TBaseObject>(obj: TTypeObject) {
   return Object.keys(obj).length === 0 && obj.constructor === Object
 }
 
-export function isArrayOfNumber(arr: any[]): boolean {
+export function isArrayOfNumber(arr: number[]): boolean {
   return arr.every((value) => typeof Number(value) === 'number' && !Number.isNaN(Number(value)))
 }
 
-export function isArrayOfCSSUnits(arr: any[]): boolean {
+export function isArrayOfCSSUnits(arr: string[]): boolean {
   const CSSUnits = /^[0-9.]+(cm|mm|Q|in|pc|pt|px|em|ex|ch|rem|lh|rlh|vw|vh|vmin|vmax|vb|vi|svw|svh|lvw|lvh|dvw|dvh)$/
   return arr.every((value) => typeof value === 'string' && CSSUnits.test(value))
 }
