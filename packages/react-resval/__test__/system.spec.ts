e4cbd9e9872873e158dda2e6ccbf34c1dd63b306
nurulakbaral <nurulakbaral@gmail.com>
2022-08-12 15:07:09 +0700

    test: add complete test for trackBreakpoints

--- packages/react-resval/__test__/system.spec.ts
+++ packages/react-resval/__test__/system.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { setBreakpoints, extendsBreakpoints, sortBreakpointsTrack } from '../src/system'
+import { setBreakpoints, extendsBreakpoints, sortBreakpointsTrack, trackBreakpoints } from '../src/system'
 import { DefaultBreakpoints } from '../src/constants'
 
 describe('Check the `setBreakpoints` utility so that it gives the expected output.', () => {
@@ -201,5 +201,115 @@ describe('Check the `sortBreakpointsTrack` utility so that it gives the expected
     expect(sortBreakpointsTrack([$500dot123px, xl, $900px, base, xs, $500dot321px, sm, md, lg, $600px])).toEqual(
       sortedFloatBreakpointsTrack,
     )
+  })
+})
+
+describe('Check the `trackBreakpoints` utility so that it gives the expected output.', () => {
+  const base = { query: 'base', constraintWidth: '0px', status: false }
+  const tbase = { query: 'base', constraintWidth: '0px', status: true }
+
+  const xs = { query: 'xs', constraintWidth: '320px', status: false }
+  const txs = { query: 'xs', constraintWidth: '320px', status: true }
+
+  const sm = { query: 'sm', constraintWidth: '576px', status: false }
+  const tsm = { query: 'sm', constraintWidth: '576px', status: true }
+
+  const $600px = { query: '600px', constraintWidth: '600px', status: false }
+  const t$600px = { query: '600px', constraintWidth: '600px', status: true }
+
+  const md = { query: 'md', constraintWidth: '768px', status: false }
+  const tmd = { query: 'md', constraintWidth: '768px', status: true }
+
+  const $900px = { query: '900px', constraintWidth: '900px', status: false }
+  const t$900px = { query: '900px', constraintWidth: '900px', status: true }
+
+  const lg = { query: 'lg', constraintWidth: '1080px', status: false }
+  const tlg = { query: 'lg', constraintWidth: '1080px', status: true }
+
+  const xl = { query: 'xl', constraintWidth: '1280px', status: false }
+  const txl = { query: 'xl', constraintWidth: '1280px', status: true }
+
+  const emptyTrack = { query: '', constraintWidth: '', status: false }
+
+  const sortedBreakpointsTrack01 = [tbase, txs, tsm, t$600px, tmd, $900px, lg, xl]
+  const sortedBreakpointsTrack02 = [tbase, txs, tsm, t$600px, md, $900px, lg, xl]
+  const sortedBreakpointsTrack03 = [tbase, txs, sm, $600px, md, $900px, lg, xl]
+  const sortedBreakpointsTrack04 = [base, xs, sm, $600px, tmd, t$900px, tlg, txl]
+  const sortedBreakpointsTrack05 = [base, xs, sm, $600px, md, t$900px, tlg, txl]
+
+  test('should return snapshot current (md) and snapshot (md) - Media (MIN)', () => {
+    expect(
+      trackBreakpoints(
+        sortedBreakpointsTrack01,
+        {
+          base: 'val: base',
+          md: 'val: md',
+        },
+        'min',
+      ),
+    ).toEqual({
+      currentBreakpoints: tmd,
+      snapshotBreakpoints: tmd,
+    })
+  })
+  test('should return snapshot current (600px) and snapshot (base) - Media (MIN)', () => {
+    expect(
+      trackBreakpoints(
+        sortedBreakpointsTrack02,
+        {
+          base: 'val: base',
+          md: 'val: md',
+        },
+        'min',
+      ),
+    ).toEqual({
+      currentBreakpoints: t$600px,
+      snapshotBreakpoints: tbase,
+    })
+  })
+  test('should return snapshot current (xs) and snapshot (empty) - Media (MIN)', () => {
+    expect(
+      trackBreakpoints(
+        sortedBreakpointsTrack03,
+        {
+          sm: 'val: sm',
+          '900px': 'vl: 900px',
+        },
+        'min',
+      ),
+    ).toEqual({
+      currentBreakpoints: txs,
+      snapshotBreakpoints: emptyTrack,
+    })
+  })
+  test('should return snapshot current (md) and snapshot (md) - Media (MAX)', () => {
+    expect(
+      trackBreakpoints(
+        sortedBreakpointsTrack04,
+        {
+          base: 'val: base',
+          md: 'val: md',
+        },
+        'max',
+      ),
+    ).toEqual({
+      currentBreakpoints: tmd,
+      snapshotBreakpoints: tmd,
+    })
+  })
+  test('should return snapshot current (900px) and snapshot (empty) - Media (MAX)', () => {
+    expect(
+      trackBreakpoints(
+        sortedBreakpointsTrack05,
+        {
+          base: 'val: base',
+          md: 'val: md',
+        },
+        'max',
+      ),
+    ).toEqual({
+      currentBreakpoints: t$900px,
+      snapshotBreakpoints: emptyTrack,
+    })
   })
 })
