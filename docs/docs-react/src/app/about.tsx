2ac109c6810ff3192335658270e973ba2dc154a6
nurulakbaral <nurulakbaral@gmail.com>
2022-08-13 08:59:10 +0700

    chore: add quick-star docs

--- docs/docs-react/src/app/about.tsx
+++ docs/docs-react/src/app/about.tsx
@@ -1,7 +1,30 @@
-export default function About() {
+import { createResponsiveValues } from '@resval/react-responsive-values'
+
+const breakpoints = {
+  base: '0px',
+  xs: '320px',
+  sm: '576px',
+  md: '768px',
+  lg: '1080px',
+  xl: '1280px',
+}
+
+export const useVx = createResponsiveValues({
+  breakpoints: { ...breakpoints },
+  media: 'min',
+})
+
+export function Component() {
+  const fontSize = useVx({ base: '12px', md: '24px' })
+  const color = useVx({ base: 'red', '600px': 'blue', lg: 'black' })
   return (
     <div>
-      <h1>Hello About</h1>
+      <h1 style={{ fontSize, color }}>Hello World!</h1>
     </div>
   )
+}
+
+export default function About() {
+  const isMobileView = useVx({ base: true, md: false })
+  return isMobileView ? <h3>Mobile View</h3> : <h1>Desktop View</h1>
 }
