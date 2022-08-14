a31b565117a84481791dd2ed99fa6c57d9602fd3
nurulakbaral <nurulakbaral@gmail.com>
2022-08-14 10:53:54 +0700

    chore: remove lint

--- docs/docs-react-e2e/src/integration/app.spec.ts
+++ docs/docs-react-e2e/src/integration/app.spec.ts
@@ -1,6 +1,5 @@
 import { TBreakpointsTrack } from './types'
 import { HEIGHT_VIEWPORT, SET_VIEWPORT } from './constants'
-/* eslint-disable cypress/no-unnecessary-waiting */
 
 describe('docs-react', () => {
   /**
