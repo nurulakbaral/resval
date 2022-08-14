fb15adc5449f9399f1bb5589915c34eb9e95edc4
nurulakbaral <nurulakbaral@gmail.com>
2022-08-14 19:13:13 +0700

    chore: add same env

--- packages/react-mq/src/react-mq.ts
+++ packages/react-mq/src/react-mq.ts
@@ -2,9 +2,65 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react'
 
-const noop = (...params: any) => {}
+const noop = () => {}
 
-const mockEnv: any = {
+const ssrDocument: any = {
+  body: {
+    classList: {
+      add() {},
+      remove() {},
+    },
+  },
+  addEventListener() {},
+  removeEventListener() {},
+  activeElement: {
+    blur() {},
+    nodeName: '',
+  },
+  querySelector() {
+    return null
+  },
+  querySelectorAll() {
+    return []
+  },
+  getElementById() {
+    return null
+  },
+  createEvent() {
+    return {
+      initEvent() {},
+    }
+  },
+  createElement() {
+    return {
+      children: [],
+      childNodes: [],
+      style: {},
+      setAttribute() {},
+      getElementsByTagName() {
+        return []
+      },
+    }
+  },
+}
+
+const ssrWindow: any = {
+  document: ssrDocument,
+  navigator: {
+    userAgent: '',
+  },
+  CustomEvent: function CustomEvent() {
+    return this
+  },
+  addEventListener: noop,
+  removeEventListener: noop,
+  getComputedStyle() {
+    return {
+      getPropertyValue() {
+        return ''
+      },
+    }
+  },
   matchMedia() {
     return {
       matches: false,
@@ -12,14 +68,36 @@ const mockEnv: any = {
       removeListener: noop,
     }
   },
-  addEventListener: noop,
-  removeEventListener: noop,
+  requestAnimationFrame(callback: () => void) {
+    if (typeof setTimeout === 'undefined') {
+      callback()
+      return null
+    }
+    return setTimeout(callback, 0)
+  },
+  cancelAnimationFrame(id: number) {
+    if (typeof setTimeout === 'undefined') return
+    clearTimeout(id)
+  },
+  setTimeout: () => 0,
+  clearTimeout: noop,
+  setInterval: () => 0,
+  clearInterval: noop,
+}
+
+const mockEnv = {
+  window: ssrWindow,
+  document: ssrDocument,
+}
+
+export function useEnvironment() {
+  return typeof window !== 'undefined' ? { window, document } : mockEnv
 }
 
 export function useMediaQueryHooks(query: string): boolean {
-  const env = typeof window !== 'undefined' ? window : mockEnv
+  const env = useEnvironment()
   const getMatches = (query: string): boolean => {
-    return env.matchMedia(query).matches
+    return env.window.matchMedia(query).matches
   }
 
   const [matches, setMatches] = React.useState<boolean>(getMatches(query))
@@ -29,7 +107,7 @@ export function useMediaQueryHooks(query: string): boolean {
   }
 
   React.useEffect(() => {
-    const matchMedia = env.matchMedia(query)
+    const matchMedia = env.window.matchMedia(query)
 
     // Triggered at the first client-side load and if query changes
     handleChange()
@@ -41,7 +119,7 @@ export function useMediaQueryHooks(query: string): boolean {
       matchMedia.removeEventListener('change', handleChange)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query])
+  }, [])
 
   return matches
 }
