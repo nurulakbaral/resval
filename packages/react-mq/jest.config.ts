c3728b88f40d22d5324bd9dbba8e4f5dfff74be8
nurulakbaral <nurulakbaral@gmail.com>
2022-08-14 16:59:07 +0700

    chore: add useMQ

---
+++ packages/react-mq/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'react-mq',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/react-mq',
+}
