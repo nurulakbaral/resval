6899085e64ec111cd4be333b52f1b50e9ce7db73
nurulakbaral <nurulakbaral@gmail.com>
2022-09-25 11:43:15 +0700

    feat: v1.0.0-beta

--- packages/react-resval/src/index.ts
+++ packages/react-resval/src/index.ts
@@ -1,9 +1,6 @@
-// Notes: Build for React Resval v1.0.0
-// Export for test only in development mode and comment out the following line if you don't want to include in the build process.
-// export * as v1 from './v1.0.0'
-// Export for build in production mode.
-export * from './v1.0.0'
+// Developement mode
+export * as v1 from './v1.0.0'
+export * from './v0.1.0'
 
-// Notes: Build for React Resval v0.1.0
-// Comment out the following line if you don't want to include in the build process.
-// export * from './v0.1.0'
+// Production mode (build process)
+// export * from './v1.0.0'
