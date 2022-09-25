1f92327aa6e3c2757734ad3be49eec8bd22fad98
nurulakbaral <nurulakbaral@gmail.com>
2022-09-25 10:28:04 +0700

    chore: add rules for build process

--- packages/react-resval/src/index.ts
+++ packages/react-resval/src/index.ts
@@ -1,3 +1,10 @@
+// Notes: Build for React Resval v1.0.0
+// Export for test only in development mode and comment out the following line if you don't want to include in the build process.
 export * as v1 from './v1'
+// Export for build in production mode.
+// export * from './v1'
+
+// Notes: Build for React Resval v0.1.0
+// Comment out the following line if you don't want to include in the build process.
 export * from './react-resval'
 export * from './types'
